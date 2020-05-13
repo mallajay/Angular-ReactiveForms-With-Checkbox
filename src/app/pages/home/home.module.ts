999c3117fcf9929e76e24e11ed45001583bdc534
Ajay Mall <mallajay19@gmail.com>
2020-05-14 00:37:43 +0530

    first commit

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,22 @@
+import { ReactiveFormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+import { IonicModule } from "@ionic/angular";
+
+import { HomePageRoutingModule } from "./home-routing.module";
+
+import { HomePage } from "./home.page";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule,
+    HomePageRoutingModule,
+  ],
+  declarations: [HomePage],
+})
+export class HomePageModule {}
